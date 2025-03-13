ad6bbc644f96fd6c369c4239b730329220f699ac
jingzhou <jingzhou_wang@hotmail.com>
2025-03-14 03:10:50 +0800

    ConnectHandler, ResponseHandler integration

--- src/main/java/com/illiad/handler/ConnectHandler.java
+++ src/main/java/com/illiad/handler/ConnectHandler.java
@@ -5,14 +5,10 @@ import com.illiad.config.Params;
 import com.illiad.security.Ssl;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.socksx.SocksMessage;
-import io.netty.handler.codec.socksx.v4.Socks4ClientDecoder;
-import io.netty.handler.codec.socksx.v4.Socks4ClientEncoder;
-import io.netty.handler.codec.socksx.v5.Socks5ClientEncoder;
-import io.netty.handler.codec.socksx.v5.Socks5CommandRequest;
-import io.netty.handler.codec.socksx.v5.Socks5CommandResponseDecoder;
+import io.netty.handler.codec.socksx.v4.*;
+import io.netty.handler.codec.socksx.v5.*;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.FutureListener;
@@ -39,80 +35,131 @@ public final class ConnectHandler extends SimpleChannelInboundHandler<SocksMessa
     @Override
     public void channelRead0(final ChannelHandlerContext ctx, final SocksMessage message) {
 
+        Bootstrap b = new Bootstrap();
+        Channel frontend = ctx.channel();
+        ChannelPipeline frontendPipeline = ctx.pipeline();
         // defind a promise to handle the connection to the remote server
         Promise<Channel> promise = ctx.executor().newPromise();
-        promise.addListener(
-                new FutureListener<>() {
-                    @Override
-                    public void operationComplete(Future<Channel> future) {
-                        final Channel frontend = ctx.channel();
-                        final ChannelPipeline frontendPipeline = ctx.pipeline();
-                        final Channel backend = future.getNow();
-                        final ChannelPipeline backendPipeline = backend.pipeline();
-                        if (future.isSuccess()) {
-                            // remove all handlers except SslHandler from backpipeline
-                            for (String name : backendPipeline.names()) {
-                                ChannelHandler handler = backendPipeline.get(name);
-                                if (!(handler instanceof SslHandler)) {
-                                    backendPipeline.remove(name);
-                                }
-                            }
 
-                            // remove all handlers from frontpipeline
-                            for (String name : frontendPipeline.names()) {
-                                frontendPipeline.remove(name);
-                            }
+        if (message instanceof Socks5CommandRequest socks5Request) {
+            promise.addListener(new FutureListener<>() {
+                @Override
+                public void operationComplete(Future<Channel> future) {
+                    final Channel backend = future.getNow();
+                    final ChannelPipeline backendPipeline = backend.pipeline();
+                    if (future.isSuccess()) {
 
-                            // setup Socks direct channel relay between frontend and backend
-                            frontendPipeline.addLast(new RelayHandler(backend, utils));
-                            backendPipeline.addLast(new RelayHandler(frontend, utils));
-                            // add SslHandler to frontpipeline
-                            ctx.channel().config().setAutoRead(true);
-                            ctx.fireChannelRead(message);
-                        } else {
-                            utils.closeOnFlush(ctx.channel());
-                            ctx.fireExceptionCaught(future.cause());
-                        }
+                        frontend.writeAndFlush(new DefaultSocks5CommandResponse(Socks5CommandStatus.SUCCESS, socks5Request.dstAddrType(), socks5Request.dstAddr(), socks5Request.dstPort()))
+                                .addListeners(future1 -> {
+                                    if (future1.isSuccess()) {
+                                        // put frontend input on-hold until both frontend and backend are ready
+                                        frontend.config().setAutoRead(false);
+                                        // remove all handlers from backpipeline
+                                        for (String name : backendPipeline.names()) {
+                                            backendPipeline.remove(name);
+                                        }
+                                        // remove all handlers except SslHandler from frontend pipeline
+                                        for (String name : frontendPipeline.names()) {
+                                            ChannelHandler handler = frontendPipeline.get(name);
+                                            if (!(handler instanceof SslHandler)) {
+                                                frontendPipeline.remove(name);
+                                            }
+                                        }
+                                        // setup Socks direct channel relay between frontend and backend
+                                        frontendPipeline.addLast(new RelayHandler(backend, utils));
+                                        backendPipeline.addLast(new RelayHandler(frontend, utils));
+                                        // resume frontend auto read
+                                        ctx.channel().config().setAutoRead(true);
+                                    } else {
+                                        utils.closeOnFlush(ctx.channel());
+                                        ctx.fireExceptionCaught(future1.cause());
+                                    }
+                                });
                     }
                 }
-        );
+            });
 
-        try {
-            Bootstrap b = new Bootstrap();
+            b.group(ctx.channel().eventLoop()).channel(NioSocketChannel.class)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ResponseHandler(promise));
+
+            // connect to the proxy server, and forward the Socks connect command message to the remote server
+            b.connect(socks5Request.dstAddr(), socks5Request.dstPort())
+                    .addListener(new ChannelFutureListener() {
+                        @Override
+                        public void operationComplete(ChannelFuture future) throws Exception {
+                            if (future.isSuccess()) {
+                                // Connection established use handler provided results
+                            } else {
+                                // Close the connection if the connection attempt has failed.
+                                ctx.channel().writeAndFlush(new DefaultSocks5CommandResponse(Socks5CommandStatus.FAILURE, socks5Request.dstAddrType()));
+                                utils.closeOnFlush(ctx.channel());
+                            }
+                        }
+                    });
+
+        } else if (message instanceof Socks4CommandRequest socks4Request) {
+            promise.addListener(new FutureListener<>() {
+                @Override
+                public void operationComplete(Future<Channel> future) {
+
+                    final Channel backend = future.getNow();
+                    final ChannelPipeline backendPipeline = backend.pipeline();
+                    if (future.isSuccess()) {
+                        frontend.writeAndFlush(new DefaultSocks4CommandResponse(Socks4CommandStatus.SUCCESS))
+                                .addListeners(future1 -> {
+                                    if (future1.isSuccess()) {
+                                        // put frontend input on-hold until both frontend and backend are ready
+                                        frontend.config().setAutoRead(false);
+                                        // remove all handlers from backpipeline
+                                        for (String name : backendPipeline.names()) {
+                                            backendPipeline.remove(name);
+                                        }
+                                        // remove all handlers except SslHandler from frontend pipeline
+                                        for (String name : frontendPipeline.names()) {
+                                            ChannelHandler handler = frontendPipeline.get(name);
+                                            if (!(handler instanceof SslHandler)) {
+                                                frontendPipeline.remove(name);
+                                            }
+                                        }
+                                        // setup Socks direct channel relay between frontend and backend
+                                        frontendPipeline.addLast(new RelayHandler(backend, utils));
+                                        backendPipeline.addLast(new RelayHandler(frontend, utils));
+                                        // resume frontend auto read
+                                        ctx.channel().config().setAutoRead(true);
+                                    } else {
+                                        utils.closeOnFlush(ctx.channel());
+                                        ctx.fireExceptionCaught(future1.cause());
+                                    }
+                                });
+                    }
+                }
+            });
+
+
             b.group(ctx.channel().eventLoop())
                     .channel(NioSocketChannel.class)
                     .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
                     .option(ChannelOption.SO_KEEPALIVE, true)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel ch) {
-                            ChannelPipeline pipeline = ch.pipeline();
-                            // Add SSL handler first to encrypt and decrypt everything.
-                            // In this example, we use a bogus certificate in the server side
-                            // and accept any invalid certificates in the client side.
-                            // You will need something more complicated to identify both
-                            // and server in the real world.
-                            pipeline.addLast(ssl.sslCtx.newHandler(ch.alloc(), params.getRemoteHost(), params.getRemotePort()),
-                                    // backend inbound decoder: standard socks5 command response or socks4 command response
-                                    message instanceof Socks5CommandRequest ? new Socks5CommandResponseDecoder() : new Socks4ClientDecoder(),
-                                    new ResponseHandler(promise),
-                                    // backend outbound encoder: a header followeed by standard socks5 command request (Connect or UdP) or sock
-                                    message instanceof Socks5CommandRequest ? Socks5ClientEncoder.DEFAULT : Socks4ClientEncoder.INSTANCE,
-                                    headerEncoder);
-                        }
-                    });
+                    .handler(new ResponseHandler(promise));
 
             // connect to the proxy server, and forward the Socks connect command message to the remote server
-            b.connect(params.getRemoteHost(), params.getRemotePort())
-                    .sync().channel()
-                    .writeAndFlush(message)
-                    .sync();
+            b.connect(socks4Request.dstAddr(), socks4Request.dstPort()).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (future.isSuccess()) {
+                        // Connection established use handler provided results
+                    } else {
+                        // Close the connection if the connection attempt has failed.
+                        ctx.channel().writeAndFlush(new DefaultSocks4CommandResponse(Socks4CommandStatus.REJECTED_OR_FAILED));
+                        utils.closeOnFlush(ctx.channel());
+                    }
+                }
+            });
 
-            ctx.pipeline().remove(this);
-        } catch (Exception e) {
-            ctx.fireExceptionCaught(e);
-        } finally {
-            utils.closeOnFlush(ctx.channel());
+        } else {
+            ctx.close();
         }
 
     }
--- src/main/java/com/illiad/handler/ResponseHandler.java
+++ src/main/java/com/illiad/handler/ResponseHandler.java
@@ -1,20 +1,14 @@
 package com.illiad.handler;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
-import io.netty.handler.codec.socksx.SocksMessage;
-import io.netty.handler.codec.socksx.SocksVersion;
-import io.netty.handler.codec.socksx.v4.Socks4CommandResponse;
-import io.netty.handler.codec.socksx.v4.Socks4CommandStatus;
-import io.netty.handler.codec.socksx.v5.Socks5CommandResponse;
-import io.netty.handler.codec.socksx.v5.Socks5CommandStatus;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.concurrent.Promise;
 
-import java.util.List;
-
-public class ResponseHandler extends ByteToMessageDecoder {
+/**
+ * attached to the backend pipeline, handls the response for the connect request to the remote server
+ */
+public class ResponseHandler extends ChannelInboundHandlerAdapter {
 
     private final Promise<Channel> promise;
 
@@ -22,49 +16,15 @@ public class ResponseHandler extends ByteToMessageDecoder {
         this.promise = promise;
     }
 
+    // trigger the promise from the frontend channel, and pass the backend channel to the promise
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> list) {
-        if (list == null || list.isEmpty() || list.get(0) == null || !(list.get(0) instanceof SocksMessage)) {
-            ctx.fireExceptionCaught(new Throwable("invalid response received from remote server"));
-        }
+    public void channelActive(ChannelHandlerContext ctx) {
+        promise.setSuccess(ctx.channel());
+    }
 
-        SocksMessage message = (SocksMessage)list.get(0);
-        Channel frontend = promise.getNow();
-        // wite response to frontend
-        frontend.writeAndFlush(message).addListener(
-                future -> {
-                    if (future.isSuccess()) {
-                        // put frontend input onhold until both frontend and backend are ready
-                        frontend.config().setAutoRead(false);
-                        // trigger promise as per message status
-                        if (message.version() == SocksVersion.SOCKS5) {
-                            if (((Socks5CommandResponse) message).status() == Socks5CommandStatus.SUCCESS) {
-                                promise.setSuccess(ctx.channel());
-                            } else {
-                                promise.setFailure(new Exception(((Socks5CommandResponse) message).status().toString()));
-                                ctx.fireExceptionCaught(new Exception(((Socks5CommandResponse) message).status().toString()));
-                            }
-
-                        } else if (message.version() == SocksVersion.SOCKS4a) {
-                            if (((Socks4CommandResponse) message).status() == Socks4CommandStatus.SUCCESS) {
-                                promise.setSuccess(ctx.channel());
-                            } else {
-                                promise.setFailure(new Exception(((Socks4CommandResponse) message).status().toString()));
-                                ctx.fireExceptionCaught(new Exception(((Socks4CommandResponse) message).status().toString()));
-                            }
-
-                        } else {
-                            promise.setFailure(new Exception(message.version().name()));
-                            ctx.fireExceptionCaught(new Exception(message.version().name()));
-                        }
-                    } else {
-                        promise.setFailure(future.cause());
-                        ctx.fireExceptionCaught(future.cause());
-                    }
-                }
-        );
-
-        ctx.pipeline().remove(this);
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable throwable) {
+        promise.setFailure(throwable);
     }
 
 }
