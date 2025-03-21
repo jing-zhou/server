7220db05d03dbbd069da90a011789697613e5b7c
jingzhou <jingzhou_wang@hotmail.com>
2025-03-21 15:27:56 +0800

    V5CommandHandler: only expect Socks5CommandRequest

--- src/main/java/com/illiad/handler/v5/V5CommandHandler.java
+++ src/main/java/com/illiad/handler/v5/V5CommandHandler.java
@@ -1,6 +1,5 @@
 package com.illiad.handler.v5;
 
-import com.illiad.codec.v5.V5CmdReqDecoder;
 import com.illiad.handler.Utils;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
@@ -10,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ChannelHandler.Sharable
-public class V5CommandHandler extends SimpleChannelInboundHandler<Socks5Message> {
+public class V5CommandHandler extends SimpleChannelInboundHandler<Socks5CommandRequest> {
     private final V5ConnectHandler connectHandler;
     private final Utils utils;
 
@@ -20,28 +19,17 @@ public class V5CommandHandler extends SimpleChannelInboundHandler<Socks5Message>
     }
 
     @Override
-    public void channelRead0(ChannelHandlerContext ctx, Socks5Message socksRequest) {
+    public void channelRead0(ChannelHandlerContext ctx, Socks5CommandRequest socksRequest) {
 
-        if (socksRequest instanceof Socks5InitialRequest) {
-            // auth support example
-            //ctx.pipeline().addFirst(new V5PwdAuthReqDecoder());
-            //ctx.write(new DefaultSocks5AuthMethodResponse(Socks5AuthMethod.PASSWORD));
-            ctx.pipeline().addFirst(new V5CmdReqDecoder());
-            ctx.write(new DefaultSocks5InitialResponse(Socks5AuthMethod.NO_AUTH));
-            //} else if (socksRequest instanceof Socks5PasswordAuthRequest) {
-            //    ctx.pipeline().addFirst(new V5CmdReqDecoder());
-            //    ctx.write(new DefaultSocks5PasswordAuthResponse(Socks5PasswordAuthStatus.SUCCESS));
-        } else if (socksRequest instanceof Socks5CommandRequest socks5CmdRequest) {
-            if (socks5CmdRequest.type() == Socks5CommandType.CONNECT) {
-                ctx.pipeline().addLast(connectHandler);
-                ctx.pipeline().remove(this);
-                ctx.fireChannelRead(socksRequest);
-            } else {
-                ctx.close();
-            }
+        // only socks5 command (connect or udp) are expected
+        if (socksRequest.type() == Socks5CommandType.CONNECT) {
+            ctx.pipeline().addLast(connectHandler);
+            ctx.pipeline().remove(this);
+            ctx.fireChannelRead(socksRequest);
         } else {
             ctx.close();
         }
+
     }
 
     @Override
