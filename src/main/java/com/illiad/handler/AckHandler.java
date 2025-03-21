d661818902f1a40e94251a9e0a601296d9a70cbc
jingzhou <jingzhou_wang@hotmail.com>
2025-03-21 14:19:38 +0800

    ResponseHandler => AckHandler

--- src/main/java/com/illiad/handler/AckHandler.java
+++ src/main/java/com/illiad/handler/AckHandler.java
@@ -8,11 +8,11 @@ import io.netty.util.concurrent.Promise;
 /**
  * attached to the backend pipeline, handls the response for the connect request to the remote server
  */
-public class ResponseHandler extends ChannelInboundHandlerAdapter {
+public class AckHandler extends ChannelInboundHandlerAdapter {
 
     private final Promise<Channel> promise;
 
-    public ResponseHandler(Promise<Channel> promise) {
+    public AckHandler(Promise<Channel> promise) {
         this.promise = promise;
     }
 
