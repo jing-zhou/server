6ea5e445896aa99b4a2dc544af4d1764589b8103
jingzhou <jingzhou_wang@hotmail.com>
2025-03-21 16:20:53 +0800

    V4ConnectHandler

--- src/main/java/com/illiad/handler/v4/V4ConnectHandler.java
+++ src/main/java/com/illiad/handler/v4/V4ConnectHandler.java
@@ -1,17 +1,12 @@
 package com.illiad.handler.v4;
 
-import com.illiad.codec.HeaderEncoder;
-import com.illiad.codec.v4.V4ClientDecoder;
-import com.illiad.codec.v4.V4ClientEncoder;
-import com.illiad.config.Params;
+import com.illiad.handler.AckHandler;
 import com.illiad.handler.RelayHandler;
 import com.illiad.handler.Utils;
-import com.illiad.security.Ssl;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.socksx.v4.Socks4CommandRequest;
+import io.netty.handler.codec.socksx.v4.*;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.util.concurrent.FutureListener;
 import io.netty.util.concurrent.Promise;
@@ -21,97 +16,65 @@ import org.springframework.stereotype.Component;
 @ChannelHandler.Sharable
 public final class V4ConnectHandler extends SimpleChannelInboundHandler<Socks4CommandRequest> {
 
-    private final Ssl ssl;
-    private final Params params;
-    private final HeaderEncoder headerEncoder;
-    private final V4ClientEncoder v4ClientEncoder;
     private final Utils utils;
-    private final Bootstrap b = new Bootstrap();
 
-    public V4ConnectHandler(Ssl ssl, Params params, HeaderEncoder headerEncoder, V4ClientEncoder v4ClientEncoder, Utils utils) {
-
-        this.ssl = ssl;
-        this.params = params;
-        this.headerEncoder = headerEncoder;
-        this.v4ClientEncoder = v4ClientEncoder;
+    public V4ConnectHandler(Utils utils) {
         this.utils = utils;
     }
 
     @Override
-    public void channelRead0(final ChannelHandlerContext ctx, final Socks4CommandRequest request) {
+    public void channelRead0(final ChannelHandlerContext ctx, final Socks4CommandRequest socks4Request) {
 
-        // define a promise to handle the connection to the remote server
+        Bootstrap b = new Bootstrap();
+        Channel frontend = ctx.channel();
+        ChannelPipeline frontendPipeline = ctx.pipeline();
+        // defind a promise to handle the connection to the remote server
         Promise<Channel> promise = ctx.executor().newPromise();
-        promise.addListener(
-                (FutureListener<Channel>) future -> {
-                    final Channel frontend = ctx.channel();
-                    final ChannelPipeline frontendPipeline = ctx.pipeline();
-                    final Channel backend = future.getNow();
-                    final ChannelPipeline backendPipeline = backend.pipeline();
-                    if (future.isSuccess()) {
-                        // remove all handlers except SslHandler from backend pipeline
-                        for (String name : backendPipeline.names()) {
-                            ChannelHandler handler = backendPipeline.get(name);
-                            if (!(handler instanceof SslHandler)) {
-                                backendPipeline.remove(name);
+        promise.addListener((FutureListener<Channel>) future -> {
+
+            final Channel backend = future.getNow();
+            final ChannelPipeline backendPipeline = backend.pipeline();
+            if (future.isSuccess()) {
+                frontend.writeAndFlush(new DefaultSocks4CommandResponse(Socks4CommandStatus.SUCCESS))
+                        .addListeners((ChannelFutureListener) future1 -> {
+                            if (future1.isSuccess()) {
+                                // remove all handlers except SslHandler from frontend pipeline
+                                for (String name : frontendPipeline.names()) {
+                                    ChannelHandler handler = frontendPipeline.get(name);
+                                    if (!(handler instanceof SslHandler)) {
+                                        frontendPipeline.remove(name);
+                                    }
+                                }
+                                // setup Socks direct channel relay between frontend and backend
+                                frontendPipeline.addLast(new RelayHandler(backend, utils));
+                                backendPipeline.addLast(new RelayHandler(frontend, utils));
+                                // resume frontend auto read
+                                frontend.config().setAutoRead(true);
+                            } else {
+                                utils.closeOnFlush(ctx.channel());
+                                ctx.fireExceptionCaught(future1.cause());
                             }
-                        }
+                        });
+            }
+        });
 
-                        // remove all handlers from frontend pipeline
-                        for (String name : frontendPipeline.names()) {
-                            frontendPipeline.remove(name);
-                        }
-
-                        // setup Socks direct channel relay between frontend and backend
-                        frontendPipeline.addLast(new RelayHandler(backend, utils));
-                        backendPipeline.addLast(new RelayHandler(frontend, utils));
-                        // restore frontend auto read
-                        ctx.channel().config().setAutoRead(true);
-                    } else {
-                        utils.closeOnFlush(ctx.channel());
-                        ctx.fireExceptionCaught(future.cause());
-                    }
-                }
-        );
 
         b.group(ctx.channel().eventLoop())
                 .channel(NioSocketChannel.class)
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
                 .option(ChannelOption.SO_KEEPALIVE, true)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel ch) {
-                        ChannelPipeline pipeline = ch.pipeline();
-                        // Add SSL handler first to encrypt and decrypt everything.
-                        // In this example, we use a bogus certificate in the server side
-                        // and accept any invalid certificates in the client side.
-                        // You will need something more complicated to identify both
-                        // and server in the real world.
-                        pipeline.addLast(ssl.sslCtx.newHandler(ch.alloc(), params.getRemoteHost(), params.getRemotePort()),
-                                // backend inbound decoder: standard socks4 command response
-                                new V4ClientDecoder(),
-                                new V4AckHandler(promise),
-                                // backend outbound encoder
-                                v4ClientEncoder,
-                                // backend header encoder
-                                headerEncoder);
-                    }
-                });
+                .handler(new AckHandler(promise));
 
         // connect to the proxy server, and forward the Socks connect command message to the remote server
-        b.connect(params.getRemoteHost(), params.getRemotePort()).channel().writeAndFlush(request)
-                .addListener(new ChannelFutureListener() {
-                    @Override
-                    public void operationComplete(ChannelFuture future) {
-                        if (future.isSuccess()) {
-                            // Connection established use handler provided results
-                        } else {
-                            // Close the connection if the connection attempt has failed.
-                            utils.closeOnFlush(ctx.channel());
-                            ctx.fireExceptionCaught(future.cause());
-                        }
-                    }
-                });
+        b.connect(socks4Request.dstAddr(), socks4Request.dstPort()).addListener((ChannelFutureListener) future -> {
+            if (future.isSuccess()) {
+                // Connection established use handler provided results
+            } else {
+                // Close the connection if the connection attempt has failed.
+                ctx.channel().writeAndFlush(new DefaultSocks4CommandResponse(Socks4CommandStatus.REJECTED_OR_FAILED));
+                utils.closeOnFlush(ctx.channel());
+            }
+        });
 
     }
 
@@ -121,3 +84,4 @@ public final class V4ConnectHandler extends SimpleChannelInboundHandler<Socks4Co
         ctx.fireExceptionCaught(cause);
     }
 }
+
