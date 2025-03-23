918abadc7f6a1493a42c29e98be77ee7478aa8bd
jingzhou <jingzhou_wang@hotmail.com>
2025-03-23 16:34:32 +0800

    Secret: secret byte

--- src/main/java/com/illiad/server/security/Secret.java
+++ src/main/java/com/illiad/server/security/Secret.java
@@ -3,14 +3,13 @@ package com.illiad.server.security;
 import com.illiad.server.config.Params;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
-
-import java.util.HexFormat;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class Secret {
     private final Params params;
     @Getter
-    private byte[] secret = HexFormat.of().parseHex("password");
+    private byte[] secret = "password".getBytes(StandardCharsets.UTF_8);
 
     public Secret(Params params) {
         this.params = params;
