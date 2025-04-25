0132de21293cb84cb199897b2d9f890b138e8fd4
jingzhou <jingzhou_wang@hotmail.com>
2025-04-25 16:44:42 +0800

    HeaderDecoder: optimize controll logic, assumedCRLF acquisition

--- src/main/java/com/illiad/server/codec/HeaderDecoder.java
+++ src/main/java/com/illiad/server/codec/HeaderDecoder.java
@@ -3,7 +3,6 @@ package com.illiad.server.codec;
 import com.illiad.server.handler.http.HttpServerHandler;
 import com.illiad.server.security.Secret;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
@@ -11,7 +10,6 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslHandler;
-import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 
@@ -41,14 +39,11 @@ public class HeaderDecoder extends ByteToMessageDecoder {
         // the first byte is assumed to be the length of the offset
         final int offsetLength = byteBuf.getByte(readerIndex);
         // Get the following 2 bytes after offset as per offsetLength, and convert into a byte array (big-endian)
-        byte[] assumedCRLF = ByteBuffer.allocate(2).putShort(byteBuf.getShort(offsetLength + 1)).array();
+        byte[] assumedCRLF = short2Bytes(byteBuf.getShort(offsetLength + 1));
 
-        // if the length of offset matches, it is an illiad header
-        if (Arrays.equals(CRLF, assumedCRLF)) {
-            // skip the offset plus 1 length byte, 2 bytes for CRLF
-            byteBuf.skipBytes(offsetLength + 3);
-        } else {
-            // not a valid illiad header, route the request to a preset https webpage
+        // if the length of offset mismatches, it is not an illiad header; route the request to a preset https webpage
+        if (!Arrays.equals(CRLF, assumedCRLF)) {
+
             // remove all handlers except SslHandler from frontendPipeline
             ChannelPipeline frontendPipeline = ctx.pipeline();
             for (String name : frontendPipeline.names()) {
@@ -69,6 +64,10 @@ public class HeaderDecoder extends ByteToMessageDecoder {
             ctx.fireChannelRead(list);
             return;
         }
+
+        // skip the offset plus 1 length byte, 2 bytes for CRLF
+        byteBuf.skipBytes(offsetLength + 3);
+
         // acquire the secret length as unsigned short
         int secretLength = byteBuf.readUnsignedShort();
         byte[] secretBytes = new byte[secretLength];
@@ -89,6 +88,13 @@ public class HeaderDecoder extends ByteToMessageDecoder {
         }
         ctx.pipeline().remove(this);
 
+    }
+
+    static byte[] short2Bytes(short value) {
+        return new byte[] {
+                (byte) (value >> 8), // Extract high byte
+                (byte) value         // Extract low byte
+        };
     }
 
 }
