dfdab6334bbda0d5565d2cdd186ffa3c08b9cff6
jingzhou <jingzhou_wang@hotmail.com>
2025-05-08 15:28:14 +0800

    Secret, SecretImp: remove uunused functions

--- src/main/java/com/illiad/server/security/Secret.java
+++ src/main/java/com/illiad/server/security/Secret.java
@@ -1,7 +1,12 @@
 package com.illiad.server.security;
 
-public interface Secret {
-    byte[] getSecret();
 
-    boolean verify(byte[] secret);
+import java.security.NoSuchAlgorithmException;
+
+public interface Secret {
+
+    short getCryptoLength(byte b);
+
+    boolean verify(byte cryptoType, byte[] secret) throws NoSuchAlgorithmException;
+
 }
---
+++ src/main/java/com/illiad/server/security/SecretImp.java
@@ -0,0 +1,32 @@
+package com.illiad.server.security;
+
+import com.illiad.server.config.Params;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+@Component
+public class SecretImp implements Secret {
+    private final Params params;
+    private final CryptoByte cryptoByte;
+
+    public SecretImp(Params params, CryptoByte cryptoByte) {
+        this.params = params;
+        this.cryptoByte = cryptoByte;
+    }
+
+    @Override
+    public short getCryptoLength(byte b) {
+        return this.cryptoByte.byteLength(this.cryptoByte.toCrypto(b));
+    }
+
+    @Override
+    public boolean verify(byte cryptoType, byte[] secret) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance(this.cryptoByte.toCrypto(cryptoType).getValue());
+        return Arrays.equals(secret, digest.digest(params.getSecret().getBytes(StandardCharsets.UTF_8)));
+    }
+
+}
