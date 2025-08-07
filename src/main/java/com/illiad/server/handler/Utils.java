c72b9ff80f7cac5a188d6d994c289dbf2b8de661
jingzhou <jingzhou_wang@hotmail.com>
2025-08-07 11:55:17 +0800

    Utils: addressType()

--- src/main/java/com/illiad/server/handler/Utils.java
+++ src/main/java/com/illiad/server/handler/Utils.java
@@ -3,6 +3,8 @@ package com.illiad.server.handler;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFutureListener;
+import io.netty.handler.codec.socksx.v5.Socks5AddressType;
+import io.netty.util.NetUtil;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +15,16 @@ public class Utils {
     public void closeOnFlush(Channel ch) {
         if (ch.isActive()) {
             ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    public Socks5AddressType addressType(String address) {
+        if (NetUtil.isValidIpV4Address(address)) {
+            return Socks5AddressType.IPv4;
+        } else if (NetUtil.isValidIpV6Address(address)) {
+            return Socks5AddressType.IPv6;
+        } else {
+            return Socks5AddressType.DOMAIN;
         }
     }
 }
