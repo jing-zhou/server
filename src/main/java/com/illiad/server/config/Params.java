2f34ba8f520baa432d944f64d79df0d2268bbb13
jingzhou <jingzhou_wang@hotmail.com>
2025-08-19 14:48:13 +0800

    Params: remove UDP relay port (automaticly assigned by OS)

--- src/main/java/com/illiad/server/config/Params.java
+++ src/main/java/com/illiad/server/config/Params.java
@@ -11,7 +11,6 @@ public class Params {
     int localPort = Integer.parseInt(System.getProperty("localPort", "2080"));
     String localHost = System.getProperty("localHost", "127.0.0.1");
     String udpHost = System.getProperty("udpHost", "127.0.0.1");
-    int udpPort = Integer.parseInt(System.getProperty("udpPort", "0"));
     String secret = "password";
 
 }
